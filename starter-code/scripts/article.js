322ec0ba9d94c3131ab5f7496aa14f311ececa5a
Alex Cahn <alexcahn@gmail.com>
2017-02-13 17:21:03 -0800

    finished last todos

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -51,8 +51,9 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(); //TODO: What do we pass in to loadAll()?
-    //TODO: What method do we call to render the index page?
+    Article.loadAll(JSON.parse(localStorage.rawData)); //TODO: What do we pass in to loadAll()?
+    //Done: What method do we call to render the index page?
+    articleView.initIndexPage();
   } else {
     console.log('loading from JSON');
     $.getJSON('./data/hackerIpsum.json')
